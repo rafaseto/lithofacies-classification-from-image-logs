8b29e8316af77b6fb7846b728ff6f97427abe10d
rafaseto <rafael08.0807@gmail.com>
2024-09-30 18:04:08 -0300

    feat: process data from ndarray to df
    Define ndarray_to_dataframe() in data_preprocess.py. Invoke function in dlis_reader.ipynb

--- src/utils/data_preprocessing.py
+++ src/utils/data_preprocessing.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 import numpy as np
+import pandas as pd
 
 def logical_files_to_ndarray(logical_files: List[object]) -> Dict[int, Dict[int, np.ndarray]]:
     """
@@ -31,3 +32,31 @@ def logical_files_to_ndarray(logical_files: List[object]) -> Dict[int, Dict[int,
         logical_file_index += 1
 
     return logical_files_dict
+
+def ndarray_to_dataframe(logical_files_dict):
+    logical_file_index = 0
+    logical_files_df_dict = {}
+
+    for logical_file in logical_files_dict.values():
+        
+        dataframe_dict = {}
+        frame_index = 0
+
+        for frame in logical_file.values():
+            i = 0
+            channel_names = frame.dtype.names
+            frame_dict = {}
+
+            for channel_name in channel_names:
+                curves = [t[i] for t in frame]
+                frame_dict[channel_name] = curves
+                i += 1
+
+            dataframe_dict[frame_index] = pd.DataFrame(frame_dict)
+            frame_index += 1
+
+        logical_files_df_dict[logical_file_index] = dataframe_dict
+        logical_file_index += 1
+
+    return logical_files_df_dict
+        
