4f0207f23e19667d3e27eab4dbcd35f03cfa81b6
rafaseto <rafael08.0807@gmail.com>
2024-09-30 16:36:48 -0300

    feat: create src and src/utils
    Create a folder for the source code of the project.

---
+++ src/utils/data_preprocessing.py
@@ -0,0 +1,33 @@
+from typing import List, Dict
+import numpy as np
+
+def logical_files_to_dict(logical_files):
+    """
+    Receives the well's logical files and creates a dictionary to store the frame data.
+
+    Args:
+        logical_files (List[object]): A list of logical file objects, each containing multiple frames.
+
+    Returns:
+        Dict[int, Dict[int, np.ndarray]]: A nested dictionary where the outer keys represent the 
+        logical file index and the inner keys represent frame indices, each associated with 
+        NumPy arrays of curve data.
+    """
+    logical_files_dict = {}
+    logical_file_index = 0
+
+    for logical_file in logical_files:
+        
+        logical_file_dict = {}
+
+        for frame in logical_file.frames:
+            frame_index = logical_file.frames.index(frame)
+
+            curves = frame.curves()
+
+            logical_file_dict[frame_index] = curves
+        
+        logical_files_dict[logical_file_index] = logical_file_dict
+        logical_file_index += 1
+
+    return logical_files_dict
