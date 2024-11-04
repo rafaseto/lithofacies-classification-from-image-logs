66fdb65feb9cd07167559ca33c4707e4c8fdeb8e
rafaseto <rafael08.0807@gmail.com>
2024-11-04 16:09:32 -0300

    feat: def create_df_subset()

--- src/utils/data_plotting.py
+++ src/utils/data_plotting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-
+from typing import Dict
 
 def plot_gr_logs(df: pd.DataFrame, title: str) -> None:
     """
@@ -439,3 +439,23 @@ def plot_gr_logs_4_runs(df_spliced: pd.DataFrame,
 
     # Display the legend for the GR log
     ax.legend()
+
+
+def create_df_subset(well_df_dict: Dict[str, Dict[int, Dict[int, pd.DataFrame]]], curve: str):
+    curve_data = {}
+    
+    for well, w_dict in well_df_dict.items():
+        curve_data[well] = {}
+
+        for logical_file, lf_dict in w_dict.items():
+            curve_data[well][logical_file] = {}
+
+            for frame, df in lf_dict.items():
+                try:
+                    curve_df = df[['TDEP', curve]]
+
+                    curve_data[well][logical_file][frame] = curve_df
+                except:
+                    pass
+                
+    return curve_data
