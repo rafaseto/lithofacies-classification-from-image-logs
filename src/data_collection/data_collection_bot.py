2f68d372ff0ba81ceb85a027b7cb4aee3e2194c8
rafaseto <rafael08.0807@gmail.com>
2025-02-24 18:01:10 -0300

    update(data_collection_bot): use chromedrive This should resolve the timeout issue

--- src/data_collection/data_collection_bot.py
+++ src/data_collection/data_collection_bot.py
@@ -3,9 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 import time
 
-driver = webdriver.Chrome()
+# Define um timeout maior para a comunicação entre o Selenium e o ChromeDriver
+CHROMEDRIVE_PATH = r"C:\Users\rafae\chromedriver-win64\chromedriver.exe"
+chrome_service = Service(CHROMEDRIVE_PATH)  # Caminho para o chromedriver
+driver = webdriver.Chrome(service=chrome_service)
+driver.set_page_load_timeout(3000)  # Timeout maior (300 segundos)
 
 BASE_URL = "https://reate.cprm.gov.br/arquivos/index.php/s/87Ny6plVJXljL0C"  
 
@@ -31,7 +37,7 @@ def navigate_and_download(caminho):
                 time.sleep(2)
                 break  
             except NoSuchElementException:
-                print(f"Element '{parte}' found. Scrolling down...")
+                print(f"Element '{parte}' not found. Scrolling down...")
                 body.send_keys(Keys.PAGE_DOWN) # Pressing the PAGE DOWN key
                 time.sleep(1)
                 attempts -= 1
@@ -60,3 +66,6 @@ def navigate_and_download(caminho):
 with open("../data_selection/selected_wells_images.txt", "r", encoding="utf-8", errors="ignore") as file:
     for path in file:
         navigate_and_download(path)
+
+input("Press Enter to close browser...")
+driver.quit()
